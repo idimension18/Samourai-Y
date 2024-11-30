954af05d626bc98db335800430510b6c9323260c
antpoms <148887171+antpoms@users.noreply.github.com>
2024-11-30 22:05:41 +0100

    Premier build
    C'est le premier build

--- Samurai-Whatever/Assets/Scripts/Enemy.cs
+++ Samurai-Whatever/Assets/Scripts/Enemy.cs
@@ -4,48 +4,57 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     private float[] sequence;
+    [SerializeField] private LevelScript level;
 
     void Start()
     {
-        GenerateSequence();
-        StartCoroutine(Shoot(2f));
+        int bpm = level.getBPM();
+        float pas = 60f/bpm;
+        StartCoroutine(Shoot(pas));
     }
 
-    void Update()
+    IEnumerator Shoot(float pas)
     {
-        
-    }
+        //metronome
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Preview2L");
+        yield return new WaitForSeconds(pas); //
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Preview2L");
+        yield return new WaitForSeconds(pas); //
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Preview2L");
+        yield return new WaitForSeconds(pas); //
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Preview2L");
+        yield return new WaitForSeconds(pas); //
 
-    void GenerateSequence()
-    {
-        switch(GlobalVariables.difficulty) {
-            case 0:
-                sequence = new float[3];
-                sequence[0] = 1f;
-                sequence[1] = 1f;
-                sequence[2] = 1f;
-                break;
-            case 1:
-                sequence[0] = 1f;
-                sequence[1] = 1.5f;
-                sequence[2] = 2f;
-                sequence[3] = 2.5f;
-                sequence[4] = 3f;
-                break;
-        }
-    }
-
-    IEnumerator Shoot(float delay)
-    {
-        for (int i = 0; i < sequence.Length; i++)
+        for (int j=0; j<level.getNbrEnnemies(); j++)
         {
-            yield return new WaitForSeconds(sequence[i]);
-            GameObject bullet = transform.GetChild(0).gameObject;
-            GameObject clone = Instantiate(bullet);
-            clone.transform.position= bullet.transform.position + Vector3.left;
-            clone.SetActive(true);
-            FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/PreviewR");
-            Debug.Log("Shoot after " + sequence[i].ToString() + " seconds");
+            sequence = level.getEnemyList()[j].getTimings();
+            Debug.Log(sequence);
+            
+            //preview du son
+            float temps_restant = 4 * pas; //temps d'attente après la dernière note de la séquence
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                yield return new WaitForSeconds(sequence[i] * pas); //
+                FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/PreviewR");
+                temps_restant -= sequence[i] * pas;
+            }
+            yield return new WaitForSeconds(temps_restant);
+            
+            //shoot
+            temps_restant = 4 * pas; //temps d'attente après la dernière note de la séquence
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                yield return new WaitForSeconds(sequence[i] * pas); //
+                GameObject bullet = transform.GetChild(0).gameObject;
+                GameObject clone = Instantiate(bullet);
+                clone.transform.position = bullet.transform.position + Vector3.left;
+                clone.SetActive(true);
+                FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/BulletR");
+                Debug.Log("Shoot after " + sequence[i].ToString() + " seconds");
+                temps_restant -= sequence[i] * pas;
+            }
+            yield return new WaitForSeconds(temps_restant);
+            //mort des ennemis
         }
     }
 
