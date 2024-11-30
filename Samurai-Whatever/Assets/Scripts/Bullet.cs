ffdd8b45ed5805a94dac34ff17bf2f94e4bdd307
XyroDp59 <81575508+XyroDp59@users.noreply.github.com>
2024-11-30 14:19:34 +0100

    bullet destroyed by player

--- Samurai-Whatever/Assets/Scripts/Bullet.cs
+++ Samurai-Whatever/Assets/Scripts/Bullet.cs
@@ -1,14 +1,31 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour
 {
-    private void UpdateScore()
+    [SerializeField] private float speed = 10f;
+    Rigidbody2D rb;
+
+    private void Awake()
     {
-        Player.score++;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    private void OnDestroy()
+    private void Start()
     {
-        UpdateScore();
+        Vector3 dir = - transform.position.normalized;
+        rb.linearVelocity = dir * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {  
+        Player player;
+        if (collision.gameObject.TryGetComponent(out player))
+        {
+            Debug.Log("GAME OVER");
+            //TODO Game Over
+            Application.Quit();
+        }
+    
     }
 }
