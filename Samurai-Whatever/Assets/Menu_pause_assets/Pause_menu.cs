ede981f99dc2446543c0f20bcff37fcba8e736be
BoutDeCiel <celidago57@gmail.com>
2024-11-30 17:19:23 +0100

    menu pause
    normalement on peut appuyer sur echap pour ouvrir ou sortir du menu mais ça marche pas donc faut appuyer sur les boutons avec la souris rip

---
+++ Samurai-Whatever/Assets/Menu_pause_assets/Pause_menu.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause_menu : MonoBehaviour
+{
+   [SerializeField] public GameObject pauseMenu;
+   
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !pauseMenu.activeSelf)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        } 
+        if (Input.GetKeyDown(KeyCode.Escape)&& pauseMenu.activeSelf)
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+}
+
